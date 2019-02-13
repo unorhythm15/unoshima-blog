1f8448b51a2d5347c9cfe725ee2761c7a2c3802b
Ryosuke Uno <uno.ryosuke@ryosuke.uno>
2019-02-14 01:28:48 +0900

    Create easy test environment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-
-    <table border="1" width="500" cellspacing="0" cellpadding="5" bordercolor="#333333">
-      <tr v-for="n in count" v-bind:key="n">
-        <td>
-          {{ n }}
-        </td>
-        <td>
-          <span v-if="n % 2 === 0">
-            {{ msg }}
-          </span>
-          <span v-else>
-            {{ msg2 }}
-          </span>
-        </td>
-      </tr>
-    </table>
+    <div>
+      <input v-model="tempCount" type="number"/>
+      <button v-on:click="syncCount"></button>
+    </div>
+    <h1>結果</h1>
+    <div>
+      <table class="table1">
+        <tr v-for="n in count" v-bind:key="n">
+          <td>
+            {{ n }}
+          </td>
+          <td>
+            <span v-if="n % 2 === 0">
+              {{ msg }}
+            </span>
+            <span v-else>
+              {{ msg2 }}
+            </span>
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -25,10 +31,30 @@ export default {
   name: 'Home',
   data () {
     return {
-      count: 20000,
+      count: 0,
+      tempCount: 0,
+      col1: 'No.',
+      col2: 'test content',
       msg: 'aaaaa',
       msg2: 'bbbbbbbb@bbbbbbbb'
+    }
+  },
+  methods: {
+    syncCount: function () {
+      this.count = Number(this.tempCount);
     }
   }
 }
 </script>
+
+<style scoped>
+td, th {
+  border: 1px grey solid;
+  width: 500px;
+  border-collapse: collapse;
+}
+button {
+  height: 19px;
+  width: 19px;
+}
+</style>
